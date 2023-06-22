6b101e8e6bd9c24ff962d6cfa8a54a34f212bd22
arutfield <36383013+arutfield@users.noreply.github.com>
2023-06-21 22:12:42 -0400

    start parsing students

--- sorting/src/main/java/classroomSorting/MainProgram.java
+++ sorting/src/main/java/classroomSorting/MainProgram.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -23,20 +24,23 @@ public class MainProgram {
 	public static void main(String[] args) throws IOException, ClassSetupException {
 		System.out.println("Hello, World: " + new java.io.File(".").getCanonicalPath()); 
 		LinkedList<Classroom> classes = new LinkedList<Classroom>();
+		LinkedList<Student> students = new LinkedList<Student>();
 		FileInputStream fis=new FileInputStream(new File("../sampleClassToArrange.xlsx"));  
 		XSSFWorkbook workbook=new XSSFWorkbook(fis);   
 		
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		boolean isInTeacherSection = false;
+		boolean isInStudentSection = false;
 		for (Row row : sheet) {
-			Classroom newClassroom = null;
 			if (row.getLastCellNum() == 0)
 				continue;
 			Cell cell = row.getCell(0);
 		    switch (cell.getCellType()) {
 		        case STRING:
-		        	if (cell.getStringCellValue().toLowerCase().equals("students"))
+		        	if (cell.getStringCellValue().toLowerCase().equals("students")) {
 		        		isInTeacherSection = false;
+		        		isInStudentSection = true;
+		        	}
 		          	if (cell.getStringCellValue().toLowerCase().equals("teachers")) {
 		           		if (!classes.isEmpty())
 		           			throw new ClassSetupException("Cannot have multiple teacher sections");
@@ -46,7 +50,7 @@ public class MainProgram {
 		           		}
 		           	}
 		          	if (isInTeacherSection) {
-	           			newClassroom = new Classroom(cell.getStringCellValue());
+	           			Classroom newClassroom = new Classroom(cell.getStringCellValue());
 	           			for (int i=1; i<row.getLastCellNum(); i++) {
 	           				Cell subCell = row.getCell(i);
 		           			if (subCell.getStringCellValue().toLowerCase().equals("ell"))
@@ -55,6 +59,19 @@ public class MainProgram {
 		           				newClassroom.enable504();
 	           			}
 			        	classes.add(newClassroom);
+		          	}
+		          	if (isInStudentSection) {
+		          		Student newStudent = new Student(cell.getStringCellValue());
+	           			for (int i=1; i<row.getLastCellNum(); i++) {
+	           				Cell subCell = row.getCell(i);
+	           				if (subCell == null)
+	           					continue;
+	           				if (subCell.getCellType() == CellType.STRING)
+	           					System.out.println("got cell: " + (subCell == null ? "nothing" : subCell.getStringCellValue()));
+	           				else if (subCell.getCellType() == CellType.NUMERIC)
+	           					System.out.println("got numeric cell: " + (subCell == null ? "nothing" : subCell.getNumericCellValue()));
+	           			}
+	           			students.add(newStudent);
 		          	}
 		            break;
 		        default:
---
+++ sorting/src/main/java/classroomSorting/Student.java
@@ -0,0 +1,11 @@
+package classroomSorting;
+
+public class Student {
+
+	private final String name;
+	
+	public Student(String studentName) {
+		name = studentName;
+	}
+
+}
