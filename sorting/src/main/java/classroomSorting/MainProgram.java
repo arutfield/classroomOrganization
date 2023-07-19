5a06a2100104ca106607d92aad7246ae684a1646
arutfield <36383013+arutfield@users.noreply.github.com>
2023-07-18 21:24:00 -0400

    handle both -a and not

--- sorting/src/main/java/classroomSorting/MainProgram.java
+++ sorting/src/main/java/classroomSorting/MainProgram.java
@@ -28,19 +28,19 @@ public class MainProgram {
 			System.exit(1);
 		}
 		SheetDissector.ParseSheet(args[0]);
-		/*Classroom[] classrooms = ClassroomSorter.solveClassrooms();
-		if (classrooms == null)
-			System.out.println("no solution found");
-		else {
-			String[] fileNameComponents = args[0].split("/");
-			String fileName = fileNameComponents[fileNameComponents.length-1];
-			
-			WriteSolution(classrooms, fileName.substring(0, fileName.length()-5));
-			for (Classroom classroom : classrooms) {
-				System.out.println(classroom.toString());
-			}
-		}*/
-		ArrayList<Classroom[]> allSolutions = ClassroomSorter.solveAllClassrooms();
+		ArrayList<Classroom[]> allSolutions = new ArrayList<Classroom[]>();
+		if (args.length > 1 && args[1].equals("-a")) {
+			allSolutions = ClassroomSorter.solveAllClassrooms();			
+		} else {
+			Classroom[] classrooms = ClassroomSorter.solveClassrooms();
+			if (classrooms != null)
+				allSolutions.add(classrooms);
+		}
+		if (allSolutions.isEmpty())
+		{
+			System.out.println("No solution found");
+			return;
+		}
 		String[] fileNameComponents = args[0].split("/");
 		String fileName = fileNameComponents[fileNameComponents.length-1];
 		WriteSolution(allSolutions, fileName.substring(0, fileName.length()-5));
@@ -71,7 +71,6 @@ public class MainProgram {
 			for (int i=0; i<classrooms.length; i++) {
 				sheet.setColumnWidth(i, 6000);
 				XSSFRow header = sheet.getRow(0);
-				
 				Cell headerCell = header.createCell(i);
 				headerCell.setCellValue(NumberReference.findTeacherNameByNumber(classrooms[i].getTeacherId()));
 				headerCell.setCellStyle(headerStyle);
@@ -85,10 +84,9 @@ public class MainProgram {
 					Cell studentHeaderCell = studentHeader.createCell(i);
 					studentHeaderCell.setCellValue(NumberReference.findStudentNameByNumber(classrooms[i].getStudentIds().get(j)));
 					studentHeaderCell.setCellStyle(studentHeaderStyle);
-				}	
+				}
 			}
 		}
-		
 		File currDir = new File(".");
 		String path = currDir.getAbsolutePath();
 		String fileLocation = path.substring(0, path.length() - 1) + fileName + "_solution.xlsx";
